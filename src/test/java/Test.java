5ca60452dbc27b88f5a61f8ede860e049837d5f1
MrVaylis <mrvaylis@icloud.com>
2024-10-17 23:18:54 +0300

    17

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,6 @@
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+//import org.telegram.telegrambots.meta.TelegramBotsApi;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+//import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Test {
   /*  public static void main(String[] args) {
